05a8b1fd1d9afa27fe9b4d8bdad37595e05633b0
Mario Rodriguez Martinez <mariog.rodriguezm@gmail.com>
2023-02-14 17:12:59 +0200

    Iteraciones 1 y 2, Done!

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,12 @@
+const mongoose = require("mongoose")
+const celebritiesSchema = new mongoose.Schema({
+    name: String,
+    occupation: String,
+    catchPhrase: String,
+
+    timestamp: true
+})
+
 //  Add your code here
+const CelebrityModel = mongoose.model("Celebrity", celebritiesSchema)
+module.exports = CelebrityModel
